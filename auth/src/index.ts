628c200a947aff18671f0a1cd3153007ab1f7f5c
Ben Alcala <benalcalajr@gmail.colm>
2022-05-02 18:02:57 -0700

    complete current user and sign out routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,4 @@
-import express, { application } from 'express';
+import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,9 +1,12 @@
-import express from 'express';
+import express, {Request, Response, NextFunction} from 'express';
+import { idText } from 'typescript';
 
 const router = express.Router();
 
-router.post('/api/users/signout', () => {
-  
+router.post('/api/users/signout', (req: Request, res: Response, next: NextFunction) => {
+  req.session = null;
+
+  res.send({})
 })
 
 export {router as signoutRouter};
