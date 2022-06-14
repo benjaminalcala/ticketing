2414b88526bf0e2cec5566bef6a73d09ba01651f
Ben Alcala <benalcalajr@gmail.colm>
2022-06-13 21:01:02 -0700

    add post route for order

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,49 @@
 import express, {Request, Response} from 'express';
+import mongoose from 'mongoose';
+import { body } from 'express-validator';
+import {requireAuth, validateRequest, NotFoundError, BadRequestError} from '@bamtickets/common';
+import { Ticket } from '../models/ticket';
+import { Order, OrderStatus } from '../models/order';
+
+const EXPIRATION_WINDOW_SECONDS = 60 * 15;
+
 
 const router  = express.Router();
 
-router.post('/api/orders', (req: Request, res: Response) => {
-  res.send({});
+router.post('/api/orders', requireAuth, [
+  body('ticketId')
+  .not()
+  .isEmpty()
+  .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+  .withMessage('Ticket id must be provided')
+],validateRequest, async (req: Request, res: Response) => {
+
+  const {ticketId} = req.body;
+
+  const ticket = await Ticket.findById(ticketId);
+
+  if(!ticket){
+    throw new NotFoundError();
+  }
+
+  const isReserved = await ticket.isReserved();
+  if(isReserved){
+    throw new BadRequestError('Ticket is already reserved');
+
+    
+  }
+
+  const expiration = new Date();
+  expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+  const order = Order.build({
+    userId: req.currentUser!.id,
+    status: OrderStatus.Created,
+    expiresAt: expiration,
+    ticket
+  })
+
+  res.status(201).send(order);
 })
 
 
