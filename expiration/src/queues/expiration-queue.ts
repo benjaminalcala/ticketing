2d8b81db3e9d65961af7a326e8e8bb0d0e108c3e
Ben Alcala <benalcalajr@gmail.colm>
2022-07-11 17:53:09 -0700

    add expiration service and create initial setup

---
+++ expiration/src/queues/expiration-queue.ts
@@ -0,0 +1,19 @@
+import Queue from 'bull';
+
+interface Payload {
+  orderId:string;
+}
+
+
+const expirationQueue = new Queue<Payload>('order:expiration', {
+  redis: {
+    host: process.env.REDIS_HOST
+  }
+});
+
+expirationQueue.process(async (job) => {
+  console.log(`Processing job ${job.data.orderId}`)
+})
+
+
+export {expirationQueue};
